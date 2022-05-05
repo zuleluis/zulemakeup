315aec46e3f24082bf8f93fe3ef9ee855b381af2
zuleluis <zulyyy123@gmail.com>
2022-05-05 01:25:06 -0500

    ClientesDAO Insercion, eliminacion y obtencion individual

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,16 @@ public class Main {
         ClientesDAO clientesDAO = new ClientesDAO(connection);
 
         //Mostrar lista de clientes
-        LinkedList<Clientes> lista = clientesDAO.getClientes();
+        //LinkedList<Clientes> lista = clientesDAO.getClientes();
+
+        //Obtiene cliente por ID
+        //Clientes cliente = clientesDAO.getCliente(1);
+
+        //Registra a un cliente
+        //Clientes cliente = new Clientes("nombre", "apPaterno", "apMaterno", "2000-11-01", "calle", 0, "colonia", "ciudad", "numTel");
+        //clientesDAO.insertaCliente(cliente);
+
+        //Elimina a un cliente
+        //Clientes cliente = clientesDAO.eliminaCliente(8);
     }
 }
--- src/main/java/datos/DAO/ClientesDAO.java
+++ src/main/java/datos/DAO/ClientesDAO.java
@@ -38,4 +38,62 @@ public class ClientesDAO {
 
         return lista;
     }
+
+    //Obtiene un cliente por ID
+    public Clientes getCliente(int idCliente) throws SQLException{
+        this.ps = this.connection.prepareStatement("SELECT * FROM Clientes WHERE idCliente = ?;");
+        this.ps.setInt(1, idCliente);
+        this.rs = this.ps.executeQuery();
+
+        if (!this.rs.next()){
+            Conexion.close(rs);
+            Conexion.close(ps);
+            return null;
+        }
+
+        Clientes auxCliente = new Clientes(rs.getString("nombre"), rs.getString("ApPaterno"), rs.getString("ApMaterno"));
+
+        Conexion.close(rs);
+        Conexion.close(ps);
+
+        System.out.println("El cliente es:" + auxCliente.getNombre() + " " + auxCliente.getApPaterno() + " " + auxCliente.getApMaterno());
+
+        return auxCliente;
+    }
+
+    //Inserta un cliente
+    public void insertaCliente(Clientes cliente) throws SQLException{
+        this.ps = this.connection.prepareStatement("INSERT INTO Clientes(nombre, apPaterno, apMaterno, fechaNac, calle, numero, colonia, ciudad, numTel) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);");
+        ps.setString(1, cliente.getNombre());
+        ps.setString(2, cliente.getApPaterno());
+        ps.setString(3, cliente.getApMaterno());
+        ps.setDate(4, java.sql.Date.valueOf(cliente.getFechaNac()));
+        ps.setString(5, cliente.getCalle());
+        ps.setInt(6, cliente.getNumero());
+        ps.setString(7, cliente.getColonia());
+        ps.setString(8, cliente.getCiudad());
+        ps.setString(9, cliente.getNumTel());
+        ps.executeUpdate();
+        //Conexion.close(ps);
+    }
+
+
+    //Elimina un cliente
+    public Clientes eliminaCliente (int idCliente) throws SQLException {
+        Clientes auxCliente = getCliente(idCliente);
+
+        if (auxCliente == null) {
+            return null;
+        }
+
+        this.ps = this.connection.prepareStatement ("DELETE FROM Clientes WHERE idCliente=?");
+
+        this.ps.setInt(1,idCliente);
+        this.ps.executeUpdate();
+
+        Conexion.close(ps);
+
+        return auxCliente;
+    }
+
 }
