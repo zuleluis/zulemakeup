ccea231b70422549004a24df4e85e4330c2884ea
zuleluis <zulyyy123@gmail.com>
2022-05-14 19:52:29 -0500

    Servlet Lista de clientes

--- src/main/java/controlador/clientes/ListaClientes.java
+++ src/main/java/controlador/clientes/ListaClientes.java
@@ -14,7 +14,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.LinkedList;
 
-@WebServlet(name = "ListaClientes", urlPatterns = {"/ListaClientes"})
+@WebServlet(name = "ListaClientes", value = "/ListaClientes")
 public class ListaClientes extends HttpServlet {
     @Resource(name = "jdbc/database")
     private DataSource conexion;
